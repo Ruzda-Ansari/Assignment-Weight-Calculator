83f434074345ed29c0f6c4d1bbebb2626698bd73
Ruzda-Ansari <75538528+Ruzda-Ansari@users.noreply.github.com>
2022-09-22 19:47:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,71 @@
-var express=require ('express');
-var app=express();
+const express=require ('express');
+const app=express();
+const bodyParser=require("body-parser");
+const mysql=require("mysql2");
+const cors = require("cors");
+const { createPool } = require('mysql');
 
-app.get('/',function(req,res){
-    res.sendFile(__dirname+'/App.js');
+const db=mysql.createPool({
+    host:"localhost",
+    user:"root",
+    password:"password",
+    database:"nodejs"
 });
 
-app.get('/API',function(req,res){
-    res.send(req.query.name);
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/api/get',(req,res) => {
+    const sqlGet=" SELECT * FROM loginuser";  
+    db.query(sqlGet,(error,result) => {
+        res.send(result);
+    });
 });
 
-app.listen(7000,()=>{
-    console.log("listening on port 7000")
+app.get('/',(req,res) => {
+    const sqlInsert="INSERT INTO loginuser(user_name,user_pass) VALUES('priya@gmail.com',123456)";
+    db.query(sqlInsert,(error,result) => {
+        console.log("error",error);
+        console.log("result",result);
+        //res.send('Hello Express');
+    }); 
 });
+
+app.listen(5000, () => {
+    console.log("server is running on 5000");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//app.get('/',function(req,res){
+  //  res.sendFile(__dirname+'/App.js');
+//});
+
+//app.get('/API',function(req,res){
+  //  res.send(req.query.name);
+//});
+
+//app.listen(7000,()=>{
+//    console.log("listening on port 7000")
+//});
 
